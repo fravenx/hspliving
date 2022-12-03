93905328ed8a4e3c78edf81ba6cccaeaf02a9a26
fengwanjie <fraven@qq.com>
2022-12-03 19:34:11 +0800

    1.0

---
+++ hspliving-commodity/src/main/java/com/hspedu/hspliving/hsplivingcommodity/HsplivingCommodityApplication.java
@@ -0,0 +1,13 @@
+package com.hspedu.hspliving.hsplivingcommodity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsplivingCommodityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HsplivingCommodityApplication.class, args);
+    }
+
+}
---
+++ hspliving-commodity/src/test/java/com/hspedu/hspliving/hsplivingcommodity/HsplivingCommodityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hspedu.hspliving.hsplivingcommodity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsplivingCommodityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
